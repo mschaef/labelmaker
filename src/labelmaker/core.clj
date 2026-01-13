553c140c20eaeab2dc73d8ad203c6fc560d0b8e2
Mike Schaeffer <mike@mschaef.com>
2026-01-12 20:01:44 -0500

    Generate an HTML file using hiccup.

--- src/labelmaker/core.clj
+++ src/labelmaker/core.clj
@@ -1,7 +1,15 @@
 (ns labelmaker.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.java.io :as io]
+            [hiccup.page :as hiccup-page]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (spit "label.html"
+        (str (hiccup-page/html5
+              [:head
+               [:style (slurp (io/resource "labelmaker.css"))]]
+              [:title "Labels"]
+              [:body
+               [:p "Hello, World!"]]))))
